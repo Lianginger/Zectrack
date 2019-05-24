892a0f919ca9e1efde88b1f05bc9a0df9ad96dec
Lianginger <jackyliang444@hotmail.com.tw>
2019-05-24 18:02:55 +0800

    Update index page

--- routes/home.js
+++ routes/home.js
@@ -3,12 +3,16 @@ const router = express.Router()
 const Project = require('../models/project')
 const Reward = require('../models/reward')
 const HourRankProject = require('../models/hour-rank-project')
+const numeral = require('numeral')
 
 router.get('/', async (req, res) => {
-  const hourRankProjects = await HourRankProject.find({})
+  let hourRankProjects = await HourRankProject.find({})
     .sort({ hourRaise: -1 })
-    .limit(5)
+    .limit(10)
     .exec()
+  hourRankProjects.map(project => {
+    project.hourRaise = numeral(project.hourRaise).format('0,0')
+  })
   res.render('index', { hourRankProjects })
 })
 
