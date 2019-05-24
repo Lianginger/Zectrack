714f17c1638e9946faddbb1b7486d8f19e9a9ad9
Lianginger <jackyliang444@hotmail.com.tw>
2019-05-24 16:51:43 +0800

    Display hour rank on index page

--- routes/home.js
+++ routes/home.js
@@ -2,9 +2,14 @@ const express = require('express')
 const router = express.Router()
 const Project = require('../models/project')
 const Reward = require('../models/reward')
+const HourRankProject = require('../models/hour-rank-project')
 
-router.get('/', (req, res) => {
-  res.render('index')
+router.get('/', async (req, res) => {
+  const hourRankProjects = await HourRankProject.find({})
+    .sort({ hourRaise: -1 })
+    .limit(5)
+    .exec()
+  res.render('index', { hourRankProjects })
 })
 
 module.exports = router
