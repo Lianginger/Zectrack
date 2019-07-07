26338c287dbc3fbcbb5b34c64766992450611b70
lianginger <jackyliang444@hotmail.com.tw>
2019-07-07 19:33:32 +0800

    Add something went wong page

--- routes/project.js
+++ routes/project.js
@@ -62,53 +62,58 @@ router.get('/:projectURI/widget', async (req, res) => {
     .sort({ date: 1 })
     .exec()
 
-  let projectInfo = projects[projects.length - 1]
-  projectInfo.raiseString = numeral(projectInfo.raise).format('0,0')
-  projectInfo.goalString = numeral(projectInfo.goal).format('0,0')
-  projectInfo.backersString = numeral(projectInfo.backers).format('0,0')
+  console.log(projects)
+  if (projects.length > 0) {
+    let projectInfo = projects[projects.length - 1]
+    projectInfo.raiseString = numeral(projectInfo.raise).format('0,0')
+    projectInfo.goalString = numeral(projectInfo.goal).format('0,0')
+    projectInfo.backersString = numeral(projectInfo.backers).format('0,0')
 
-  let dateAxisData = []
-  let raiseAxisData = []
-  let backerAxisData = []
-  let rankAxisData = []
-  projects.map(project => {
-    dateAxisData.push(project.date)
-    raiseAxisData.push(project.raise)
-    backerAxisData.push(project.backers)
-    rankAxisData.push(parseInt(project.rank))
-  })
+    let dateAxisData = []
+    let raiseAxisData = []
+    let backerAxisData = []
+    let rankAxisData = []
+    projects.map(project => {
+      dateAxisData.push(project.date)
+      raiseAxisData.push(project.raise)
+      backerAxisData.push(project.backers)
+      rankAxisData.push(parseInt(project.rank))
+    })
 
-  let dailyRaiseData = []
-  let yesterdayRaise = 0
-  raiseAxisData.map(todayRaise => {
-    dailyRaiseData.push(todayRaise - yesterdayRaise)
-    yesterdayRaise = todayRaise
-  })
+    let dailyRaiseData = []
+    let yesterdayRaise = 0
+    raiseAxisData.map(todayRaise => {
+      dailyRaiseData.push(todayRaise - yesterdayRaise)
+      yesterdayRaise = todayRaise
+    })
 
-  let dailyBackerData = []
-  let yesterdaybacker = 0
-  backerAxisData.map(todayBacker => {
-    dailyBackerData.push(todayBacker - yesterdaybacker)
-    yesterdaybacker = todayBacker
-  })
+    let dailyBackerData = []
+    let yesterdaybacker = 0
+    backerAxisData.map(todayBacker => {
+      dailyBackerData.push(todayBacker - yesterdaybacker)
+      yesterdaybacker = todayBacker
+    })
 
-  let dateAxisDataString = JSON.stringify(dateAxisData)
-  let raiseAxisDataString = JSON.stringify(raiseAxisData)
-  let dailyRaiseDataString = JSON.stringify(dailyRaiseData)
-  let backerAxisDataString = JSON.stringify(backerAxisData)
-  let dailyBackerDataString = JSON.stringify(dailyBackerData)
-  let rankAxisDataString = JSON.stringify(rankAxisData)
+    let dateAxisDataString = JSON.stringify(dateAxisData)
+    let raiseAxisDataString = JSON.stringify(raiseAxisData)
+    let dailyRaiseDataString = JSON.stringify(dailyRaiseData)
+    let backerAxisDataString = JSON.stringify(backerAxisData)
+    let dailyBackerDataString = JSON.stringify(dailyBackerData)
+    let rankAxisDataString = JSON.stringify(rankAxisData)
 
-  res.render('widget', {
-    layout: 'widget-layout',
-    projectInfo,
-    dateAxisDataString,
-    raiseAxisDataString,
-    dailyRaiseDataString,
-    backerAxisDataString,
-    dailyBackerDataString,
-    rankAxisDataString
-  })
+    res.render('widget', {
+      layout: 'widget-layout',
+      projectInfo,
+      dateAxisDataString,
+      raiseAxisDataString,
+      dailyRaiseDataString,
+      backerAxisDataString,
+      dailyBackerDataString,
+      rankAxisDataString
+    })
+  } else {
+    res.render('not-found')
+  }
 })
 
 module.exports = router
